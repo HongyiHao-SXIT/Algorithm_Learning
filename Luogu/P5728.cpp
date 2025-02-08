8075b0be6d4940ea7be85861361952719df0313b
HongyiHao-SXIT <Lanyi_adict@outlook.com>
2025-02-09 00:16:40 +0800

    code in 2.9

--- Luogu/P5728.cpp
+++ Luogu/P5728.cpp
@@ -9,14 +9,14 @@ int cnt;
 
 int main () {
     cin >> n;
-    for(int i = 0; i <n; n++){
+    for(int i = 0; i <n; i++){
         cin>>arr[i][0]>>arr[i][1]>>arr[i][2];
         sum[i] = arr[i][0] + arr[i][1] + arr[i][2]; 
     }
 
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(abs (arr[i][1] - arr[j][1]) <= 5 && abs (arr[i][2] - arr[j][2]) <= 5 && abs (arr[i][3] - arr[j][3]) <= 5 && abs (sum[i] - sum[j]) <= 10){
+        for(int j = i + 1; j < n; j++){
+            if(abs (arr[i][0] - arr[j][0]) <= 5 && abs (arr[i][1] - arr[j][1]) <= 5 && abs (arr[i][2] - arr[j][2]) <= 5 && abs (sum[i] - sum[j]) <= 10){
                 cnt++;
             }
         }
